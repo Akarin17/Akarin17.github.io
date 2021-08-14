99372e8823b4da95ee7855dea39d778fe4b82134
Akarin17 <febriff050@gmail.com>
2021-08-14 08:35:25 +0700

    hadeh

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,6 +39,12 @@ sr.reveal('.button',{delay: 200});
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
+/*SCROLL HOBBY*/
+sr.reveal('.hobby__subtitle',{}); 
+sr.reveal('.hobby__text',{}); 
+sr.reveal('.hobby__data',{interval: 200}); 
+sr.reveal('.hobby__img',{delay: 600});
+
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
 sr.reveal('.about__subtitle',{delay: 400}); 
@@ -52,10 +58,4 @@ sr.reveal('.skills__img',{delay: 600});
 
 /*SCROLL WORK*/
 sr.reveal('.work__img',{interval: 200}); 
-
-/*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
-
-
-
 
